9cce62f84eb6d40a0ab322166dd6a7320c54d6b0
Sulthan Alif <sulthanalif45@gmail.com>
2023-11-06 17:07:11 +0700

    asd

---
+++ Boolean-Expression/BooleanExpression.java
@@ -0,0 +1,35 @@
+/**
+ * BooleanExpression
+ */
+
+ import java.util.Scanner;
+
+public class BooleanExpression {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+
+        boolean a, b, c, d, e, f;
+        double x, y;
+    
+        System.out.print ("Give me two numbers. First: ");
+        x = keyboard.nextDouble() ;
+        System.out.print("Second: ");
+        y = keyboard. nextDouble () ;
+
+        a = (x < y);
+        b = (x <= y);
+        c = (x == y);
+        d = (x != y);
+        e = (x > y);
+        f = (x >= y);
+
+        System.out.println(x + " is LESS THAN " + y + ": * "+ a);
+        System.out.println(x + " is LESS THAN / EQUAL TO * "+ y +" = = "+ b);
+        System.out.println(x + " is EQUAL TO " + y + "= * + c");
+        System.out.println(x + " is NOT EQUAL TO " + y +" *= * "+ d);
+        System.out.println(x + " is GREATER THAN " + y + ": • "+ e);
+        System.out.println(x + " 1s GREATER THAN / EQUAL TO * "+ y + ": " + f);
+        System.out.println ();
+
+    }
+}
